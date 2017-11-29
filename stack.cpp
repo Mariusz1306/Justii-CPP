a05619a42ca2489b02e5bb155656d3e674bebedb
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2017-11-29 09:53:46 +0100

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "stack.h"
+#define pSTACKSIZE 8
+int ssize = pSTACKSIZE;
+
+stack::stack()
+{
+	this->top=0;
+	this->dane=(int *)malloc(ssize*sizeof(int));
+}
+
+stack::~stack()
+{
+    free(this->dane);
+}
+
+void stack::clear()
+{
+	this->top=0;
+}
+
+void stack::push(int a)
+{
+	if (this->top>=ssize)
+        ssize *= 2;
+        this->dane=(int*)realloc(this->dane,ssize*sizeof(int));
+	this->dane[this->top++]=a;
+}
+
+int stack::pop()
+{
+	if(this->top==0){
+	this->~stack();
+	assert(this->top>0);}
+	return this->dane[--this->top];
+}
+
+bool stack::isEmpty(){
+	return (this->top==0 ? true : false);
+}
+
